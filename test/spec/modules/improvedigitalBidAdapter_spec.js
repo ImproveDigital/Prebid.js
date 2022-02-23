04222efecc26ab1a8daf41db156b49757ffd07dd
Faisal Islam <faisal.islam@vivacomsolutions.com>
2022-02-23 14:06:42 +0600

    HBT-170: Written a couple of test cases

--- test/spec/modules/improvedigitalBidAdapter_spec.js
+++ test/spec/modules/improvedigitalBidAdapter_spec.js
@@ -566,25 +566,6 @@ describe('Improve Digital Adapter Tests', function () {
       expect(payload.imp[0].ext.gpid).to.not.exist;
       expect(payload.imp[0].instl).to.not.exist;
     });
-
-    it('should set coppa', function() {
-      sinon.stub(config, 'getConfig')
-        .withArgs('coppa')
-        .returns(true);
-      const bidRequest = Object.assign({}, simpleBidRequest);
-      const request = spec.buildRequests([bidRequest], bidderRequestReferrer)[0];
-      const params = JSON.parse(decodeURIComponent(request.data.substring(PARAM_PREFIX.length)));
-      expect(params.bid_request.coppa).to.equal(1);
-
-      config.getConfig.restore();
-    });
-
-    it('should undefined coppa', function() {
-      const bidRequest = Object.assign({}, simpleBidRequest);
-      const request = spec.buildRequests([bidRequest], bidderRequestReferrer)[0];
-      const params = JSON.parse(decodeURIComponent(request.data.substring(PARAM_PREFIX.length)));
-      expect(params.bid_request.coppa).to.equal(undefined);
-    });
   });
 
   const serverResponse = {
@@ -618,33 +599,6 @@ describe('Improve Digital Adapter Tests', function () {
           'seat': 'improvedigital'
         }
       ]
-    }
-  };
-
-  const serverResponseRazr = {
-    'body': {
-      'id': '687a06c541d8d1',
-      'site_id': 191642,
-      'bid': [
-        {
-          'isNet': false,
-          'id': '33e9500b21129f',
-          'advid': '5279',
-          'price': 1.45888594164456,
-          'nurl': 'https://ice.360yield.com/imp_pixel?ic=wVmhKI07hCVyGC1sNdFp.6buOSiGYOw8jPyZLlcMY2RCwD4ek3Fy6.xUI7U002skGBs3objMBoNU-Frpvmb9js3NKIG0YZJgWaNdcpXY9gOXE9hY4-wxybCjVSNzhOQB-zic73hzcnJnKeoGgcfvt8fMy18-yD0aVdYWt4zbqdoITOkKNCPBEgbPFu1rcje-o7a64yZ7H3dKvtnIixXQYc1Ep86xGSBGXY6xW2KfUOMT6vnkemxO72divMkMdhR8cAuqIubbx-ZID8-xf5c9k7p6DseeBW0I8ionrlTHx.rGosgxhiFaMqtr7HiA7PBzKvPdeEYN0hQ8RYo8JzYL82hA91A3V2m9Ij6y0DfIJnnrKN8YORffhxmJ6DzwEl1zjrVFbD01bqB3Vdww8w8PQJSkKQkd313tr-atU8LS26fnBmOngEkVHwAr2WCKxuUvxHmuVBTA-Lgz7wKwMoOJCA3hFxMavVb0ZFB7CK0BUTVU6z0De92Q.FJKNCHLMbjX3vcAQ90=',
-          'h': 290,
-          'pid': 1053688,
-          'sync': [
-            'https://link1',
-            'https://link2'
-          ],
-          'crid': '422031',
-          'w': 600,
-          'cid': '99006',
-          'adm': 'document.writeln("<script async src=\"https:\/\/cdn.inskinad.com\/isfe\/tags\/dsp.js\"><\/script>\n<script type=\"text\/javascript\">\n      (function() {\n        var ns = window.inskin = window.inskin || {};\n        ns.dsp = ns.dsp || [];\n        ns.dsp.push({\n          uri: \"razr:\/\/provider\/format\"\n        });\n      })();\n<\/script>");document.writeln("<improvedigital_ad_output_information tp_id=\"\" buyer_id=\"0\" rtb_advertiser=\"\" campaign_id=\"123456\" line_item_id=\"123456\" creative_id=\"123456\" crid=\"0\" placement_id=\"123456\"><\/improvedigital_ad_output_information>");'
-        }
-      ],
-      'debug': ''
     }
   };
 
@@ -1163,16 +1117,6 @@ describe('Improve Digital Adapter Tests', function () {
       expect(bids[0].renderer).to.exist;
       delete (bids[0].renderer);
       expect(bids).to.deep.equal(expectedBidOutstreamVideo);
-    });
-
-    it('should not affect non-RAZR bids', function () {
-      const bids = spec.interpretResponse(serverResponse, {bidderRequest});
-      expect(bids[0].ad).to.not.include('razrBidId');
-    });
-
-    it('should detect RAZR bids', function () {
-      const bids = spec.interpretResponse(serverResponseRazr, {bidderRequest});
-      expect(bids[0].ad).to.include('razrBidId');
     });
   });
 
